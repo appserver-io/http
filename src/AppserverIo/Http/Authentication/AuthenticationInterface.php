03c1ea4c1c1558931e1ce837ebe7c9008ce23dc5
Tim Wagner <t.wagner@techdivision.com>
2016-01-08 19:13:17 +0100

    Refactoring authorization integration

--- src/AppserverIo/Http/Authentication/AuthenticationInterface.php
+++ src/AppserverIo/Http/Authentication/AuthenticationInterface.php
@@ -21,11 +21,15 @@
 
 namespace AppserverIo\Http\Authentication;
 
+use AppserverIo\Psr\HttpMessage\RequestInterface;
+use AppserverIo\Psr\HttpMessage\ResponseInterface;
+
 /**
- * Interface AuthenticationInterface
+ * Interface for specific authentication type implementations.
  *
- * @author    Johann Zelger <jz@appserver.io>1
+ * @author    Johann Zelger <jz@appserver.io>
  * @author    Bernahrd Wick <bw@appserver.io>
+ * @author    Tim Wagner <tw@appserver.io>
  * @copyright 2015 TechDivision GmbH <info@appserver.io>
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link      https://github.com/appserver-io/http
@@ -35,47 +39,62 @@ interface AuthenticationInterface
 {
 
     /**
-     * Constructs the authentication type
+     * Initialize by the authentication type with the data from the request.
      *
-     * @param array $configData The configuration data for auth type instance
-     */
-    public function __construct(array $configData = array());
-
-    /**
-     * Initialise by the auth content got from client
-     *
-     * @param string $rawAuthData The content of authentication data sent by client
-     * @param string $reqMethod   The https request method as string
+     * @param \AppserverIo\Psr\HttpMessage\RequestInterface  $request  The request with the content of authentication data sent by client
+     * @param \AppserverIo\Psr\HttpMessage\ResponseInterface $response The response sent back to the client
      *
      * @return void
+     * @throws \AppserverIo\Http\Authentication\AuthenticationException If the authentication type can't be initialized
      */
-    public function init($rawAuthData, $reqMethod);
+    public function init(RequestInterface $request, ResponseInterface $response);
 
     /**
-     * Try to authenticate
+     * Try to authenticate against the configured adapter.
      *
-     * @return bool If auth was successful return true if no false will be returned
+     * @param \AppserverIo\Psr\HttpMessage\ResponseInterface $response The response sent back to the client
+     *
+     * @return void
+     * @throws \AppserverIo\Http\Authentication\AuthenticationException Is thrown if the request can't be authenticated
      */
-    public function authenticate();
+    public function authenticate(ResponseInterface $response);
 
     /**
-     * Returns the authentication type token to compare with request header
+     * Returns the authentication type token to compare with request header.
      *
      * @return string
      */
     public function getType();
 
     /**
-     * Returns the authentication header for response to set
+     * Returns the authentication header for response to set.
      *
      * @return string
      */
     public function getAuthenticateHeader();
 
     /**
-     * Returns the parsed username
+     * Returns the parsed username.
      *
      * @return string
      */
     public function getUsername();
+
+    /**
+     * Add's a new adapter type to this authentication type.
+     *
+     * @param string $adapterType The supported adapter type
+     *
+     * @return void
+     */
+    public function addSupportedAdapter($adapterType);
+
+    /**
+     * Whether or not the adapter is supported with a this authentication type
+     *
+     * @param string $adapterType The adapter type
+     *
+     * @return boolean
+     */
+    public function isAdapterSupported($adapterType);
 }
